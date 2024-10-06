3aa15af3f49ba8e6fd6e7658331dc8c31af470f1
SiHyun <there7788@gmail.com>
2024-10-06 21:48:01 +0900

    Fix: Joi 라이브러리 내 모든 메서드를 불러오기 위해 import 변경
    - 발생한 문제: 서버 run 시 Joi 라이브러리 내의 object 메서드를 찾을 수 없는 문제 발생
    - 원인: Joi 라이브러리를 각 메서드를 직접 불러오지 않아서 인식을 못함
    - 기존: import Joi from 'joi';
    - 변경: Joi 라이브러리 내에 있는 모든 메서드를 불러오기 위해 import * as Joi from 'joi';로 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiaryModule } from './diary/diary.module';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
