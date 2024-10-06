c9ad28aaa509b27760aeaec9273b0821936b97ef
SiHyun <there7788@gmail.com>
2024-10-06 21:05:12 +0900

    Feature: 다이어리 작성 로직 제어를 위한 컨트롤러 작성

--- src/diary/diary.controller.ts
+++ src/diary/diary.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { DiaryService } from './diary.service';
 import { CreateDiaryDto } from './dto/create-diary.dto';
 import { UpdateDiaryDto } from './dto/update-diary.dto';
+import { DiaryEntity } from './entities/diary.entity';
 
 @Controller('diary')
 export class DiaryController {
   constructor(private readonly diaryService: DiaryService) {}
 
   @Post()
-  create(@Body() createDiaryDto: CreateDiaryDto) {
+  create(@Body() createDiaryDto: CreateDiaryDto): Promise<DiaryEntity> {
     return this.diaryService.create(createDiaryDto);
   }
 
