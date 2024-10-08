98e8298ec07119ac91a20c782d38a4a9827b7c12
SiHyun <there7788@gmail.com>
2024-10-08 19:23:01 +0900

    Chore: 보안, CORS, cookie 설정
    - 보안: helmet 적용
    - cookie: cookie-parser 적용
    - CORS: enableCors 안에 origin, credentials, exportHeaders, preflight, methods 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    exposedHeaders: ['Authorization'],
+    preflightContinue: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  });
   await app.listen(4000);
 }
 bootstrap();
